56b67ff7bfe41a3f9d843908c7701636d92d99ca
hisoka <1399952343@qq.com>
2022-08-07 09:37:36 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Mangosteen/dist/",
+  // base: "/Mangosteen/dist/",  // 部署到github使用
   plugins: [vue()],
 });
